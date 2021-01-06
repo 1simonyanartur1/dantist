44fc7a633e455ea4e600a4ec6aa437030e2c0c43
Artur <1simonyanartur1@gmail.com>
2021-01-07 01:36:08 +0300

    working on main

--- app/js/common.js
+++ app/js/common.js
@@ -54,6 +54,26 @@
 			}
 		});
 
+		$('.doctors .slider').slick({
+			dots: false,
+			prevArrow: '<button class="arrow arrow-prev"><svg viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.7071 8.70711C21.0976 8.31658 21.0976 7.68342 20.7071 7.2929L14.3431 0.928933C13.9526 0.538409 13.3195 0.538409 12.9289 0.928933C12.5384 1.31946 12.5384 1.95262 12.9289 2.34315L18.5858 8L12.9289 13.6569C12.5384 14.0474 12.5384 14.6805 12.9289 15.0711C13.3195 15.4616 13.9526 15.4616 14.3431 15.0711L20.7071 8.70711ZM-8.74228e-08 9L20 9L20 7L8.74228e-08 7L-8.74228e-08 9Z"/></svg></button>',
+			nextArrow: '<button class="arrow arrow-next"><svg viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.7071 8.70711C21.0976 8.31658 21.0976 7.68342 20.7071 7.2929L14.3431 0.928933C13.9526 0.538409 13.3195 0.538409 12.9289 0.928933C12.5384 1.31946 12.5384 1.95262 12.9289 2.34315L18.5858 8L12.9289 13.6569C12.5384 14.0474 12.5384 14.6805 12.9289 15.0711C13.3195 15.4616 13.9526 15.4616 14.3431 15.0711L20.7071 8.70711ZM-8.74228e-08 9L20 9L20 7L8.74228e-08 7L-8.74228e-08 9Z"/></svg></button>',
+			asNavFor: '.metro .slider-nav',
+			infinite: true,
+			speed: 300,
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			responsive: [
+				{
+					breakpoint: 700,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 1,
+					}
+				}
+			]
+		});
+
 		var $status = $('.metro .slider-counter');
 		var $slickElement = $('.metro .slider-for');
 
